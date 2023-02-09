a2122078b532e0d38823d8da10ea7afdaa8e7181
smriti700 <smritigoel700@gmail.com>
2023-02-09 15:19:42 +0530

    commit with code

--- inheritance.py
+++ inheritance.py
@@ -1,12 +1,25 @@
 class Shape:
     def __init__(self):
      self.area = 0
-     self.colour = ""
+     self.colour = "blue"
 
 class Triangle(Shape):
     def tri_area(self, b, h):
         self.area = 0.5*b*h
-        return self.area;
+        return self.area
+
+class Rectangle(Shape):
+    def rec_area(self, l, b):
+        self.area = l*b
+        return self.area
+
+tri1 = Triangle()
+print(tri1.tri_area(4, 10), tri1.colour)
+
+rec1 = Rectangle()
+print(rec1.rec_area(4, 10), rec1.colour)
+
+
     
 
 
